59ec1db05bd04a8b14893eb282f2b4fbfe38584a
GT <league@WTS14-4.attlocal.net>
2018-12-20 19:56:23 -0800

    started encapsulate

--- src/_01_introduction_to_encapsulation/EncapsulateTheData.java
+++ src/_01_introduction_to_encapsulation/EncapsulateTheData.java
@@ -15,20 +15,60 @@ package _01_introduction_to_encapsulation;
  * 
  * */
 
-
 public class EncapsulateTheData {
-	//1. Encapsulate the member variables.
-	//   Add restrictions to the setters according to the comment.
-	
-	//2. Create a new JUnit Test case and write tests to verify that 
-	//   the member variables' getters and setters are working
-	
-	int itemsReceived; //must not be negative. All negative arguments get set to 0.
-	float degreesTurned; //must be locked between 0.0 and 360.0 inclusive.
-	String nomenclature = " "; //must not be set to a blank string. Blank Strings get set to a space
-	Object memberObj; //can be any object type except String. Strings get turned into objects.
-	
+	// 1. Encapsulate the member variables.
+	// Add restrictions to the setters according to the comment.
+
+	// 2. Create a new JUnit Test case and write tests to verify that
+	// the member variables' getters and setters are working
+
+	private int itemsReceived; // must not be negative. All negative arguments get set to 0.
+	private float degreesTurned; // must be locked between 0.0 and 360.0 inclusive.
+	private String nomenclature = " "; // must not be set to a blank string. Blank Strings get set to a space
+	private Object memberObj; // can be any object type except String. Strings get turned into objects.
+
 	public static void main(String[] args) {
-		
+
+	}
+
+	int getItemsReceived() {
+		return itemsReceived;
+	}
+
+	void setItemsReceived(int itemsReceived) {
+		if (itemsReceived < 0) {
+			this.itemsReceived = 0;
+		} else {
+			this.itemsReceived = itemsReceived;
+		}
+	}
+
+	float getDegreesTurned() {
+		return degreesTurned;
+	}
+
+	void setDegreesTurned(float degreesTurned) {
+		if (0.0 <= degreesTurned && degreesTurned <= 360.0) {
+			this.degreesTurned = degreesTurned;
+		}
+
+	}
+
+	String getNomenclature() {
+		return nomenclature;
+	}
+
+	void setNomenclature(String nomenclature) {
+		if (nomenclature.length() == 0) {
+			this.nomenclature = nomenclature;
+		}
+	}
+
+	Object getMemberObj() {
+		return memberObj;
+	}
+
+	void setMemberObj(Object memberObj) {
+		this.memberObj = memberObj;
 	}
 }
