51468ea710ddade53f57e3ea5c530596af48a176
Your Name <you@example.com>
2019-02-28 20:00:41 -0800

    yeah

--- src/Checkpoint.java
+++ src/Checkpoint.java
@@ -1,4 +1,37 @@
 
 public class Checkpoint {
+	// Given the following class:
+
+	// public abstract class Cake {
+	// protected String name;
+	// protected double rate;
+	//
+	// public Cake(String n, double r) {
+	// name = n;
+	// rate = r;
+	// }
+	//
+	// public abstract double calcPrice();
+	//
+	// public String toString() {
+	// return name + “\t” + rate;
+	// }
+	// }
+
+	// A) Based on class Cake and the following table, define TWO (2) subclasses
+	// named as
+	// orderCake
+	// and
+	// readymadeCake.
+
+	//
+	// B) By using classes definition from a), write an application program that
+	// will:
+	// i) declare an array of 20 cake objects;
+	// ii) input data for cake objects and store them into the array;
+	// iii) display the total price for all types of cakes;
+	// iv) display the total price and the quantity sold for ready made cakes;
+	// v) display the information for the cake that has been sold for the highest
+	// price.
 
 }
